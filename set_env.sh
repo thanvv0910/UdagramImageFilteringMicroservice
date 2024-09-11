3fbfcea03fcf043afeaaa98a694aca93d5855c4c
Vu Viet Than <thanvv0910@gmail.com>
2024-09-11 23:55:17 +0700

    update

--- set_env.sh
+++ set_env.sh
@@ -1,10 +1,10 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=thanvv2
-export POSTGRES_PASSWORD=PasswordUdacity
-export POSTGRES_HOST=cdr.ck32awz0nqcv.us-east-1.rds.amazonaws.com
+export POSTGRES_PASSWORD=vietthan
+export POSTGRES_HOST=database-thanvv2.cjdfezjue4zy.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=thanvv2-s3-bucket-cloud-pj3
+export AWS_BUCKET=arn:aws:s3:::s3-bucket-thanvv2
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=testing
